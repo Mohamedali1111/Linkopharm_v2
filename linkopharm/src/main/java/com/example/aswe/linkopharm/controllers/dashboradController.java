039ffe64aa3f8fe42c0cf5b0287febd928f2b9ff
EslamAhmedv <125676587+EslamAhmedv@users.noreply.github.com>
2024-03-06 19:09:46 +0200

    Create dashboradController.java

---
+++ linkopharm/src/main/java/com/example/aswe/linkopharm/controllers/dashboradController.java
@@ -0,0 +1,14 @@
+package com.example.aswe.linkopharm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class dashboradController {
+
+    @GetMapping("dashboard")
+    public String dashboard(){
+        return "dashboard";
+
+    }
+}
