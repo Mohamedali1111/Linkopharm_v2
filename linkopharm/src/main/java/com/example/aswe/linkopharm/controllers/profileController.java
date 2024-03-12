05b584619a2551beb0d19d6b80a0162e60b1a26f
DanielMichel03 <daniel2101026@miuegypt.edu.eg>
2024-03-12 17:58:28 +0200

    user profile

--- linkopharm/src/main/java/com/example/aswe/linkopharm/controllers/profileController.java
+++ linkopharm/src/main/java/com/example/aswe/linkopharm/controllers/profileController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class cartController {
+public class profileController {
     
-    @GetMapping("cart")
-    public String cart(){
-        return "cart";
+    @GetMapping("profile")
+    public String profile(){
+        return "profile";
 
     }
 }
