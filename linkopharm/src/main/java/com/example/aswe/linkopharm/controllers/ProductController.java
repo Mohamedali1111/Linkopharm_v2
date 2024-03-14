6bbf6a13042ccf3af8e17b2f3644c32b343dc687
EslamAhmedv <125676587+EslamAhmedv@users.noreply.github.com>
2024-03-14 21:15:15 +0200

    add product to database

--- linkopharm/src/main/java/com/example/aswe/linkopharm/controllers/ProductController.java
+++ linkopharm/src/main/java/com/example/aswe/linkopharm/controllers/ProductController.java
@@ -1,43 +1,40 @@
 package com.example.aswe.linkopharm.controllers;
 
-import java.util.List;
-
+import com.example.aswe.linkopharm.models.products;
+import com.example.aswe.linkopharm.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.aswe.linkopharm.models.products;
-import com.example.aswe.linkopharm.repository.ProductRepository;
-
-@RestController
+@Controller
 @RequestMapping("/products")
 public class ProductController {
 
     @Autowired
-    private ProductRepository  productRepository;
+    private ProductRepository productRepository;
+
+    @GetMapping("")
+    public ModelAndView getProducts() {
+        ModelAndView mav = new ModelAndView("displayproducts");
+        mav.addObject("products", productRepository.findAll());
+        return mav;
+    }
 
     @GetMapping("/add")
     public ModelAndView addProductForm() {
-        ModelAndView mav = new ModelAndView("addproducts"); 
-        mav.addObject("product", new products());
+        ModelAndView mav = new ModelAndView("addproducts");
+        mav.addObject("product", new products()); 
         return mav;
     }
 
-    @PostMapping("/add")
-    public ModelAndView addProductSubmit(@ModelAttribute products product) {
+    @PostMapping("/save")
+    public ModelAndView saveProduct(@ModelAttribute products product) {
         productRepository.save(product);
-        return new ModelAndView("redirect:/products/all");
-    }
-
-    @GetMapping("/all")
-    public ModelAndView getAllProducts() {
-        ModelAndView mav = new ModelAndView("displayproducts"); 
-        List<products> productList = productRepository.findAll();
-        mav.addObject("products", productList);
-        return mav;
+        
+        return new ModelAndView("redirect:/products");
     }
 }
