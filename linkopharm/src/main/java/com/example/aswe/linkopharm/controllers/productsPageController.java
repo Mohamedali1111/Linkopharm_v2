dc24f7d23c9972995a2f4c275a86f56839c2394f
Youssef <youssef1352019@icloud.com>
2024-04-30 09:54:21 +0300

    Dynamic productDetails page

---
+++ linkopharm/src/main/java/com/example/aswe/linkopharm/controllers/productsPageController.java
@@ -0,0 +1,34 @@
+package com.example.aswe.linkopharm.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.aswe.linkopharm.models.products;
+import com.example.aswe.linkopharm.repositories.ProductRepository;
+
+@Controller
+public class productsPageController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/productsPage/view")
+    public String getProducts(Model model) {
+        model.addAttribute("products", productRepository.findAll());
+        return "productsPage";
+    }
+
+    @GetMapping("/productsPage/view/productDetails/{productId}")
+    public String getProductDetails(@PathVariable("productId") Integer productId, Model model) {
+        products product = this.productRepository.getById(productId);
+        model.addAttribute("product", product);
+        return "productDetails";
+    }
+
+}
+
+
+
