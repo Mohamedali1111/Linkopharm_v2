b9fa56a157f3fd23070b7a216c1cb2a5e30420b9
Youssef <youssef1352019@icloud.com>
2024-05-06 09:41:47 +0300

    Modify checkout controller

--- linkopharm/src/main/java/com/example/aswe/linkopharm/controllers/checkoutController.java
+++ linkopharm/src/main/java/com/example/aswe/linkopharm/controllers/checkoutController.java
@@ -1,37 +1,29 @@
 package com.example.aswe.linkopharm.controllers;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
 
 import com.example.aswe.linkopharm.models.order;
 import com.example.aswe.linkopharm.repositories.orderRepository;
 
 
+
 @RestController 
 @RequestMapping("/checkout")
 public class checkoutController {
-    //   @Autowired
-    // private orderRepository orderRepository;
-
-    // @PostMapping("placeOrder")
-    //     public String addOrder(@ModelAttribute order order) {
-    //     this.orderRepository.save(order);
-    //     return "Added";
-    // }
-
-    // @ExceptionHandler(Exception.class)
-    // public ModelAndView handleException(Exception e) {
-    //     ModelAndView mav = new ModelAndView("error");
-    //     mav.addObject("message", "An error occurred: " + e.getMessage());
-    //     return mav;
-    // }
-
+    
+    @Autowired
+    private orderRepository orderRepository;
 
     @GetMapping("")
     public ModelAndView checkout() {
@@ -40,8 +32,30 @@ public class checkoutController {
         return mav;
     }
 
-}
+    @PostMapping("/placeOrder")
+    public String addOrder(@ModelAttribute order order) {
+        order.setStatus("Pending");
+        order.setOrderDate(LocalDate.now().toString());
+        this.orderRepository.save(order);
+        // int orderId = order.getId();
+        // return new ModelAndView("redirect:/checkout/confirmationPage/" + orderId);
+        return "Added";
+    }
 
+    // @GetMapping("/confirmationPage/{orderId}")
+    // public String getOrderDetails(@PathVariable("orderId") Integer orderId, Model model) {
+    //     order order = this.orderRepository.getById(orderId);
+    //     model.addAttribute("order", order);
+    //     return "confirmationPage";
+    // }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleException(Exception e) {
+        ModelAndView mav = new ModelAndView("error");
+        mav.addObject("message", "An error occurred: " + e.getMessage());
+        return mav;
+    }
+}
 
 
 
