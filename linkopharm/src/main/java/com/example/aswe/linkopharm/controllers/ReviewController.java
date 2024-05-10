87fd478e3bc3030d0af46a605f166b1a891ce73c
EslamAhmedv <125676587+EslamAhmedv@users.noreply.github.com>
2024-05-10 19:08:50 +0300

    admin can delete a review

--- linkopharm/src/main/java/com/example/aswe/linkopharm/controllers/ReviewController.java
+++ linkopharm/src/main/java/com/example/aswe/linkopharm/controllers/ReviewController.java
@@ -2,8 +2,14 @@ package com.example.aswe.linkopharm.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.aswe.linkopharm.models.Review;
 import com.example.aswe.linkopharm.repositories.ReviewRepository;
@@ -24,4 +30,19 @@ public class ReviewController {
         reviewRepository.save(review);
         return "redirect:/";
     }
+
+    @GetMapping("/reports")
+    public ModelAndView showReports() {
+        ModelAndView modelAndView = new ModelAndView("reports");
+        Iterable<Review> reviews = reviewRepository.findAll();
+        modelAndView.addObject("reviews", reviews);
+        return modelAndView;
+    }
+
+    @PostMapping("/delete-review")
+    public ModelAndView deleteReview(@RequestParam Long id) {
+        reviewRepository.deleteById(id);
+        return new ModelAndView("redirect:/reports");
+    }
+
 }
