3191471b15dbdecbfa408c9a5fafaf857e358d5c
Youssef <youssef1352019@icloud.com>
2024-05-12 22:38:51 +0300

    Update order repository

--- linkopharm/src/main/java/com/example/aswe/linkopharm/repositories/orderRepository.java
+++ linkopharm/src/main/java/com/example/aswe/linkopharm/repositories/orderRepository.java
@@ -3,7 +3,12 @@ package com.example.aswe.linkopharm.repositories;
 import com.example.aswe.linkopharm.models.order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface orderRepository extends JpaRepository<order, Integer> {
+
+    Optional<order> findTopByUserIdOrderByOrderDateDesc(int userId);
+
 }
+
