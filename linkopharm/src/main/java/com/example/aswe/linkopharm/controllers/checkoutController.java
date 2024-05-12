3bb42941e9af68072f43c1b590191f4e858f5792
Youssef <youssef1352019@icloud.com>
2024-05-12 22:36:16 +0300

    Update checkout controller

--- linkopharm/src/main/java/com/example/aswe/linkopharm/controllers/checkoutController.java
+++ linkopharm/src/main/java/com/example/aswe/linkopharm/controllers/checkoutController.java
@@ -1,20 +1,24 @@
 package com.example.aswe.linkopharm.controllers;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.ui.Model;
 
+import com.example.aswe.linkopharm.models.User;
 import com.example.aswe.linkopharm.models.order;
+import com.example.aswe.linkopharm.repositories.UserRepository;
 import com.example.aswe.linkopharm.repositories.orderRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 
 
@@ -25,6 +29,9 @@ public class checkoutController {
     @Autowired
     private orderRepository orderRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("")
     public ModelAndView checkout() {
         ModelAndView mav = new ModelAndView("checkout"); 
@@ -33,21 +40,43 @@ public class checkoutController {
     }
 
     @PostMapping("/placeOrder")
-    public String addOrder(@ModelAttribute order order) {
-        order.setStatus("Pending");
-        order.setOrderDate(LocalDate.now().toString());
-        this.orderRepository.save(order);
-        // int orderId = order.getId();
-        // return new ModelAndView("redirect:/checkout/confirmationPage/" + orderId);
-        return "Added";
+    public ModelAndView addOrder(@ModelAttribute order order ,User user,HttpSession session) {
+        user = this.userRepository.findByEmail((String) session.getAttribute("email"));
+        if (user != null) {
+            order.setUserId( user.getId());
+            order.setTotalPrice(1800.0);
+            order.setStatus("Pending");
+
+            // Set the order date with time
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String formattedDateTime = now.format(formatter);
+            order.setOrderDate(formattedDateTime);
+
+            this.orderRepository.save(order);
+            return new ModelAndView("redirect:/checkout/confirmationPage");
+        } else {
+            return new ModelAndView("redirect:/User/Login?error=user_not_found");
+        }
     }
 
-    // @GetMapping("/confirmationPage/{orderId}")
-    // public String getOrderDetails(@PathVariable("orderId") Integer orderId, Model model) {
-    //     order order = this.orderRepository.getById(orderId);
-    //     model.addAttribute("order", order);
-    //     return "confirmationPage";
-    // }
+    @GetMapping("/confirmationPage")
+    public ModelAndView getOrderDetails(HttpSession session) {
+        User user = this.userRepository.findByEmail((String) session.getAttribute("email"));
+        if (user != null) {
+            Optional<order> optionalOrder = this.orderRepository.findTopByUserIdOrderByOrderDateDesc(user.getId());
+            if (optionalOrder.isPresent()) {
+                order lastOrder = optionalOrder.get();
+                ModelAndView modelAndView = new ModelAndView("confirmationPage");
+                modelAndView.addObject("order", lastOrder);
+                return modelAndView;
+            } else {
+                return new ModelAndView("redirect:/errorPage");
+            }
+        } else {
+            return new ModelAndView("redirect:/errorPage");
+        }
+    }
 
     @ExceptionHandler(Exception.class)
     public ModelAndView handleException(Exception e) {
@@ -56,6 +85,7 @@ public class checkoutController {
         return mav;
     }
 }
+
 
 
 
