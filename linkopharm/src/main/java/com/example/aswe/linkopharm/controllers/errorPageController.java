f3957ab87ca36897a79668b2556a71e986b4b2dd
Youssef <youssef1352019@icloud.com>
2024-05-16 19:44:35 +0300

    Update errorPage controller

--- linkopharm/src/main/java/com/example/aswe/linkopharm/controllers/errorPageController.java
+++ linkopharm/src/main/java/com/example/aswe/linkopharm/controllers/errorPageController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.aswe.linkopharm.models.error;
+
 @Controller 
 @RequestMapping("/errorPage")
 public class errorPageController {
@@ -12,8 +14,26 @@ public class errorPageController {
     @GetMapping("")
     public ModelAndView error(){
         ModelAndView mav = new ModelAndView("errorPage.html");
+        error error = new error();
+        error.setErrortitle("404 - Page Not Found");
+        error.setErrorMessage("Oops! The page you're looking for could not be found.");
+        mav.addObject("error", error);
+        return mav;
+    }
+
+    @GetMapping("/emptyCart")
+    public ModelAndView emptyCartError(){
+        ModelAndView mav = new ModelAndView("errorPage.html");
+        error error = new error();
+        error.setErrortitle("Error");
+        error.setErrorMessage("Oops! Your cart is empty. Please add items before placing an order.");
+        mav.addObject("error", error);
         return mav;
     }
 }
+
+
+
+
 
 
