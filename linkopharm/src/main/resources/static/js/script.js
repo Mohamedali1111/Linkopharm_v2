8d52fe4613f2cff6ffe73d010d01bf7ae43d0afe
DanielMichel03 <daniel2101026@miuegypt.edu.eg>
2024-05-20 14:42:47 +0300

    orderDash searchbar

--- linkopharm/src/main/resources/static/js/script.js
+++ linkopharm/src/main/resources/static/js/script.js
@@ -37,3 +37,31 @@ AOS.init({
 
 
 
+function searchTable() {
+    console.log("AAAAAAAAAAA");
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        let matchFound = false;
+        let tdElements = tr[i].getElementsByTagName("td");
+        for (let j = 1; j < tdElements.length; j++) {
+          td = tdElements[j];
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            matchFound = true;
+            break;
+          }
+        }
+        if (matchFound) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+  }
