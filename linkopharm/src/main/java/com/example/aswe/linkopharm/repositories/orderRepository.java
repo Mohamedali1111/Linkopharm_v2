4709ca421e2f04bdb612875e85c2d55572dc4130
Youssef <youssef1352019@icloud.com>
2024-05-26 22:30:30 +0300

    Update order repository

--- linkopharm/src/main/java/com/example/aswe/linkopharm/repositories/orderRepository.java
+++ linkopharm/src/main/java/com/example/aswe/linkopharm/repositories/orderRepository.java
@@ -1,7 +1,13 @@
 package com.example.aswe.linkopharm.repositories;
 
 import com.example.aswe.linkopharm.models.order;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +19,11 @@ public interface orderRepository extends JpaRepository<order, Integer> {
     Optional<order> findTopByUserIdOrderByOrderDateDesc(int userId);
 
     List<order> findByUserId(int userId);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE orders SET status = 'Cancelled' WHERE id = :orderId AND status = 'PENDING'", nativeQuery = true)
+    int cancelOrder(@Param("orderId") Integer orderId);
 
 }
 
